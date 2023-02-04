a6d9f810c312589d09b58f72c48c24dd43a82965
tbarry2 <tbact528@hotmail.com>
2023-02-03 20:21:04 -0500

    added all essential tasks to deploy contract on windows

--- scripts/createWallet.bat
+++ scripts/createWallet.bat
@@ -7,6 +7,13 @@ CALL %~dp0setEnv
 
 rem create wallet
 solana-keygen new --no-bip39-passphrase --outfile %walletsDir%/%1.json
+rem Optionally set default wallet
+if "%~2"=="--not-default" (
+	echo --not-default specified this wallet will not be set as the default wallet
+) else (
+	echo --not-default was not specified so this will be set as the default wallet
+	CALL solana config set --keypair "%walletsDir%/%1.json"
+)
 rem set pubKey variable to the result of the command to get the public address
 FOR /F "tokens=* USEBACKQ" %%F IN (`solana-keygen pubkey %walletsDir%/%1.json`) DO (
 SET pubkey=%%F
