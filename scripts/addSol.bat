c76d859eaf24d75afe7fb6b8293eaccfcb3b2421
tbarry2 <tbact528@hotmail.com>
2023-02-03 22:10:56 -0500

    let user specify sol amount to add

--- scripts/addSol.bat
+++ scripts/addSol.bat
@@ -10,7 +10,4 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`solana-keygen pubkey %walletsDir%/%1.json`) 
 SET pubkey=%%F
 )
 
-rem Docs say max of only 1 sol per request but smart contract says max of 2
-rem Docs say 1 sol per request smart contract technically allows 2 per request
-rem but it seems when asking for 2 you get rate limited faster
-solana airdrop 1 %pubkey% --url https://api.devnet.solana.com
+solana airdrop %2 %pubkey% --url https://api.devnet.solana.com
